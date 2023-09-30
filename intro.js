6904b4b5f27cb8055e174d5226cbe02c44cddda2
MdMizanurRahman1 <mizan.rahman2217@gmail.com>
2023-09-30 16:26:32 +0300

    first commit

---
+++ intro.js
@@ -0,0 +1,12 @@
+"use strict";
+// number
+Object.defineProperty(exports, "__esModule", { value: true });
+var myNum = 234;
+var num = 256;
+console.log(myNum + num);
+// string
+var greetings = 'Hello, How are you ?';
+console.log(greetings);
+// boolean
+var isPlaying = true;
+console.log(isPlaying);
---
+++ intro.ts
@@ -0,0 +1,24 @@
+// number
+
+let myNum: number = 234;
+let num: number = 256;
+
+console.log(myNum + num);
+
+// string
+
+let greetings: string = 'Hello, How are you ?'
+console.log(greetings);
+
+
+// boolean
+
+let isPlaying: boolean = true;
+
+console.log(isPlaying);
+
+
+
+
+
+export { }
